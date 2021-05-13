07370c47f4f844eb9799787bfa6cc3a192158b36
ddomagoj <ddespala@gmail.com>
2021-05-13 20:26:39 +0200

    helper for job show with _form_application

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,14 @@
 module JobsHelper
+
+  def show_application_form(job)
+     if job.job_applied?(current_user)
+      content_tag(:h3, "You applied for this job", class: "alert alert-warning")
+     elsif job.job_expiered?
+      content_tag(:h3,  "This job expiered", class: "alert alert-warning")
+    elsif job.job_belong?(current_user)
+      content_tag(:h3,  "You created this job ", class: "alert alert-warning")
+    else
+      render partial: 'form_application', job: job
+     end 
+    end
 end
